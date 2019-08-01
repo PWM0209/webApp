a122781896909a1f91a7ac46414a6b89641737b3
PWM0209 <pengwm0209@hotmail.com>
2019-08-01 14:52:21 +0800

    update

--- static/app/home/home.js
+++ static/app/home/home.js
@@ -1,6 +1,6 @@
 /**
  * home controller
  */
-pages.controller("homeCtrl",function(){
-    console.log("--> homeCtrl");
+pages.controller("homeCtrl",function($rootScope){
+    console.log("--> homeCtrl",$rootScope.$state);
 });
